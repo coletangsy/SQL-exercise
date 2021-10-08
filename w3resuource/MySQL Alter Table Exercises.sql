6c251f04245257b183d0fa6f1ddc3c0677b99e27
Nicole Tang S. Y <tsycole@gmail.com>
2021-10-08 22:03:36 +0800

    Upload Via Github Desktop

---
+++ w3resuource/MySQL Alter Table Exercises.sql
@@ -0,0 +1,44 @@
+-- 1. Write a SQL statement to rename the table countries to country_new.
+
+
+-- . Write a SQL statement to add a column region_id to the table locations.
+
+
+-- 3. Write a SQL statement to add a columns ID as the first column of the table locations.
+
+
+-- 4. Write a SQL statement to add a column region_id after state_province to the table locations.
+
+
+-- 5. Write a SQL statement change the data type of the column country_id to integer in the table locations.
+
+
+-- 6. Write a SQL statement to drop the column city from the table locations.
+
+
+-- 7. Write a SQL statement to change the name of the column state_province to state, keeping the data type and size same.
+
+
+-- 8. Write a SQL statement to add a primary key for the columns location_id in the locations table.
+
+
+-- 9. Write a SQL statement to add a primary key for a combination of columns location_id and country_id.
+
+
+-- 10. Write a SQL statement to drop the existing primary from the table locations on a combination of columns location_id and country_id.
+
+
+-- 11. Write a SQL statement to add a foreign key on job_id column of job_history table referencing to the primary key job_id of jobs table.
+
+
+-- 12. Write a SQL statement to add a foreign key constraint named fk_job_id on job_id column of job_history table referencing to the primary key job_id of jobs table.
+
+
+-- 13. Write a SQL statement to drop the existing foreign key fk_job_id from job_history table on job_id column which is referencing to the job_id of jobs table.
+
+
+-- 14. Write a SQL statement to add an index named indx_job_id on job_id column in the table job_history.
+
+
+-- 15. Write a SQL statement to drop the index indx_job_id from job_history table.
+
--- w3resuource/MySQL Update Table Exercises.sql
+++ w3resuource/MySQL Update Table Exercises.sql
@@ -4,11 +4,11 @@ SET EMAIL = "not available";
 
 -- 2. Write a SQL statement to change the email and commission_pct column of employees table with 'not available' and 0.10 for all employees.
 UPDATE employees
-SET EMAIL = "not available" AND commission_pct = 0.10;
+SET EMAIL = "not available" , commission_pct = 0.10;
 
 -- 3. Write a SQL statement to change the email and commission_pct column of employees table with 'not available' and 0.10 for those employees whose department_id is 110.
 UPDATE employees
-SET EMAIL = "not available" AND commission_pct = 0.10
+SET EMAIL = "not available" , commission_pct = 0.10
 WHERE department_id = 110;
 
 -- 4. Write a SQL statement to change the email column of employees table with 'not available' for those employees whose department_id is 80 and gets a commission is less than .20%
@@ -21,15 +21,37 @@ UPDATE employees
 SET EMAIL = "not available"
 WHERE JOB_ID = "FI_ACCOUNT";
 
+-- correct:
+UPDATE employees
+SET EMAIL = "not available"
+WHERE department_id = (
+	SELECT department_id
+    FROM departments
+    WHERE department_name = "Accounting");
+
 -- 6. Write a SQL statement to change salary of employee to 8000 whose ID is 105, if the existing salary is less than 5000.
 UPDATE employees
 SET SALARY = 8000
-WHERE EMPLOYEE_ID = 105 & SALARY <5000;
+WHERE EMPLOYEE_ID = 105 AND SALARY <5000;
 
 -- 7. Write a SQL statement to change job ID of employee which ID is 118, to SH_CLERK if the employee belongs to department, which ID is 30 and the existing job ID does not start with SH.
-
+UPDATE employees
+SET JOB_ID = SH_CLERK
+WHERE EMPLOYEE_ID = 118 AND DEPARTMENT_ID = 30 AND JOB_ID NOT LIKE "SH%";
 
 -- 8. Write a SQL statement to increase the salary of employees under the department 40, 90 and 110 according to the company rules that, salary will be increased by 25% for the department 40, 15% for department 90 and 10% for the department 110 and the rest of the departments will remain same.
-
+UPDATE employees
+SET SALARY = CASE DEPARTMENT_ID
+		WHEN 40 THEN SALARY + SALARY * 0.25
+		WHEN 90 THEN SALARY + SALARY * 0.15
+		WHEN 110 THEN SALARY + SALARY * 0.10
+		ELSE SALARY
+    END
+	WHERE DEPARTMENT_ID IN (40,90,110);
+    
 -- 9. Write a SQL statement to increase the minimum and maximum salary of PU_CLERK by 2000 as well as the salary for those employees by 20% and commission percent by .10.
-
+UPDATE employees, jobs
+SET jobs.min_salary = jobs.min_salary + 2000, jobs.max_salary = jobs.max_salary + 2000,
+employees.salary = employees.salary + employees.salary * 0.20,
+employees.commission_pct = employees.commission_pct + 0.10
+WHEN employees.JOB_ID = "PU_CLERK" AND jobs.id = "PU_CLERK";
